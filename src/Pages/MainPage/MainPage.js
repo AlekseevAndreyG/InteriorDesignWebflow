3faeeefb75a2b30c42c0e324b67799b9f90d1b85
AlekseevAndreyG <dr.erklide@yandex.ru>
2023-09-12 16:29:49 +0400

    Исправил все пути, страница MainPage готова

--- src/Pages/MainPage/MainPage.js
+++ src/Pages/MainPage/MainPage.js
@@ -1,16 +1,16 @@
 import React from "react";
 import Hero from "./components/Hero/Hero";
-import Plan from "./componenets/Plan/Plan";
+import Plan from "./components/Plan/Plan";
 import PlanMobile from "./components/Plan/PlanMobile/PlanMobile";
 import AboutUs from "./components/AboutUs/AboutUs";
-import AboutUsMobile from "./componenets/AboutUs/AboutUsMobile/AboutUsMobile";
+import AboutUsMobile from "./components/AboutUs/AboutUsMobile/AboutUsMobile";
 import Reviews from "./components/Reviews/Reviews";
 import OurPartners from "./components/OurPartners/OurPartners";
+import OurPartnersMobile from "./components/OurPartners/OurPartnersMobile/OurPartnersMobile";
 import FollowOurProjects from "./components/FollowOurProjects/FollowOurProjects";
 import FollowOurProjectMobile from "./components/FollowOurProjects/FollowOurProjectMobile/FollowOurProjectMobile";
 import CounterTotal from "./components/CounterTotal/CounterTotal";
 import Blog from "./components/Blog/Blog";
-import OurPartnersMobile from "./components/OurPartners/OurPartnersMobile/OurPartnersMobile";
 import WannaJoin from "./components/WannaJoin/WannaJoin";
 
 const MainPage = () => {
--- src/Pages/MainPage/components/FollowOurProjects/FollowOurProjects.js
+++ src/Pages/MainPage/components/FollowOurProjects/FollowOurProjects.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import "./FollowOurProjects.css";
-import FollowOurProjectsData from "../../../../Arrays/FollowOurProjectsData";
+import FollowOurProjectsData from "../../../../components/Arrays/FollowOurProjectsData";
 
 function FollowOurProjects() {
   useEffect(() => {
