0aedbeac3a6008fb7f6a9b1c5a0ceb807483b38a
AlekseevAndreyG <dr.erklide@yandex.ru>
2023-09-12 14:05:20 +0400

    обернул app в brouser router

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import "./fonts/DMSerifDisplay-Regular.ttf";
 import "./fonts/DMSerifDisplay-Italic.ttf";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
