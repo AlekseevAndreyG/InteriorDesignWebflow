ca12f4eb98ba48f69db076dbb30a07b6dac2bd49
AlekseevAndreyG <dr.erklide@yandex.ru>
2023-09-13 17:05:54 +0400

    Сделал AboutCart для pc и начал делать AboutCartMobile

--- src/Pages/AboutPage/components/AboutCarts/AboutCart.js
+++ src/Pages/AboutPage/components/AboutCarts/AboutCart.js
@@ -1,7 +1,48 @@
 import React from "react";
 import "./AboutCart.css";
 function AboutCart() {
-  return <div></div>;
+  return (
+    <section className="about-cart">
+      <div className="container">
+        <div className="about-cart-wrap">
+          <div className="about-cart-cat1">
+            <div className="about-cart-cat1-item1">
+              <h2>What We Do</h2>
+              <p>
+                It is a long established fact that a reader will be distracted
+                by the of readable content of a page when lookings at its
+                layouts the points of using that it has a more-or-less normal.
+              </p>
+              <button>
+                <span>Our Concept</span>
+                <img src="img/interface/coffe-vector.svg" alt="arrow"></img>
+              </button>
+            </div>
+            <div className="about-cart-cat1-item2">
+              <img src="img/about-page/we-do.jpg" alt="img"></img>
+            </div>
+          </div>
+          <div className="about-cart-cat2">
+            <div className="about-cart-cat2-item1">
+              <h2>The End Result</h2>
+              <p>
+                It is a long established fact that a reader will be distracted
+                by the of readable content of a page when lookings at its
+                layouts the points of using that it has a more-or-less normal.
+              </p>
+              <button>
+                <span>Our Portfolio</span>
+                <img src="img/interface/coffe-vector.svg" alt="arrow"></img>
+              </button>
+            </div>
+            <div className="about-cart-cat2-item2">
+              <img src="img/about-page/end-result.jpg" alt="img"></img>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default AboutCart;
---
+++ src/Pages/AboutPage/components/AboutCarts/AboutCartMobile/AboutCartMobile.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./AboutCartMobile.css"
+function AboutCartMobile() {
+  return (
+    <div>AboutCartMobile</div>
+  )
+}
+
+export default AboutCartMobile
