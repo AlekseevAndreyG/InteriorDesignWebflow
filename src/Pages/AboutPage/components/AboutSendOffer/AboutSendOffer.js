c1ada36a7fe45e6fa24c26134ffdd26c028c80b7
AlekseevAndreyG <dr.erklide@yandex.ru>
2023-09-15 18:29:04 +0400

    Закончил секцию отправки и поменял цвет текста поиска в хедере

--- src/Pages/AboutPage/components/AboutSendOffer/AboutSendOffer.js
+++ src/Pages/AboutPage/components/AboutSendOffer/AboutSendOffer.js
@@ -1,7 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import "./AboutSendOffer.css";
+
 function AboutSendOffer() {
-  return <div></div>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Добавьте здесь код для отправки письма
+    console.log("Имя:", name);
+    console.log("Email:", email);
+    console.log("Сообщение:", message);
+    // Сбросить значения полей формы после отправки
+    setName("");
+    setEmail("");
+    setMessage("");
+  };
+
+  return (
+    <section className="about-send-offer">
+      <div className="container">
+        <h2>
+          Creative project?<br></br> Let's have a productive talk.
+        </h2>
+        <div className="about-send-offer-form">
+          <form className="about-send-offer-form" onSubmit={handleSubmit}>
+            <div className="about-send-offer-form-fields">
+              <div className="form-field form-name">
+                <label htmlFor="name">Name:</label>
+                <input
+                  type="text"
+                  id="name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="form-field form-email">
+                <label htmlFor="email">Email:</label>
+                <input
+                  type="email"
+                  id="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </div>
+            </div>
+            <div className="form-field form-message">
+              <label htmlFor="message">Hello I am Interested in..</label>
+              <textarea
+                id="message"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                required
+              ></textarea>
+            </div>
+            <button type="submit">
+              <p>Send Now</p>
+              <img src="img/interface/coffe-vector.svg" alt="arrow"></img>
+            </button>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default AboutSendOffer;
---
+++ src/components/Arrays/Designers.js
@@ -0,0 +1,40 @@
+const designers = [
+  {
+    number: 1,
+    name: "Michael Smith",
+    country: "USA",
+    phone: "+1 (378) 400-1234",
+    email: "michael@email.com",
+    image: "img/avatar/liders/design1.jpg",
+    photo: "img/designer-photo/design1-photo.jpg",
+  },
+  {
+    number: 2,
+    name: "Raichel Springfield",
+    country: "Australia",
+    phone: "+1 (378) 355-1222",
+    email: "raichel@email.com",
+    image: "img/avatar/liders/design2.jpg",
+    photo: "img/designer-photo/design2-photo.jpg",
+  },
+  {
+    number: 3,
+    name: "Andrew Garcia",
+    country: "Mexico",
+    phone: "+1 (378) 307-1119",
+    email: "andrew@email.com",
+    image: "img/avatar/liders/design3.jpg",
+    photo: "img/designer-photo/design3-photo.jpg",
+  },
+  {
+    number: 4,
+    name: "Milana Hill",
+    country: "Australia",
+    phone: "+1 (378) 355-1222",
+    email: "milana@email.com",
+    image: "img/avatar/liders/design4.jpg",
+    photo: "img/designer-photo/design4-photo.jpg",
+  },
+];
+
+export default designers;
