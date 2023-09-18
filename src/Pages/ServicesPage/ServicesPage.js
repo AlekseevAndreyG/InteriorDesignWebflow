ee57a55cd6fb4cee97d547c8a672c05fd1fb5365
AlekseevAndreyG <dr.erklide@yandex.ru>
2023-09-18 17:08:03 +0400

    Начал делать секцию ServicesHowWeWork

--- src/Pages/ServicesPage/ServicesPage.js
+++ src/Pages/ServicesPage/ServicesPage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ServicesHeading from "./components/SevicesHeading/ServicesHeading";
+import ServicesHowWeWork from "./components/ServicesHowWeWork/ServicesHowWeWork";
 
 function ServicesPage() {
   return (
     <section className="services-page">
       <ServicesHeading />
+      <ServicesHowWeWork />
     </section>
   );
 }
---
+++ src/Pages/ServicesPage/components/ServicesHowWeWork/ServicesHowWeWork.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./ServicesHowWeWork.css";
+function ServicesHowWeWork() {
+  return <section></section>;
+}
+
+export default ServicesHowWeWork;
