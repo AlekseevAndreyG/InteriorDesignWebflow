6ff5618edce20f6e5706a075c40c27e6437ae39a
AlekseevAndreyG <dr.erklide@yandex.ru>
2023-09-18 18:34:49 +0400

    почти закончил секцию, осталось адаптив

--- src/Pages/ServicesPage/components/ServicesHowWeWork/ServicesHowWeWork.js
+++ src/Pages/ServicesPage/components/ServicesHowWeWork/ServicesHowWeWork.js
@@ -1,7 +1,48 @@
 import React from "react";
+import ServicesHowWeWorkData from "../../../../components/Arrays/ServicesHowWeWorkData";
 import "./ServicesHowWeWork.css";
-function ServicesHowWeWork() {
-  return <section></section>;
+
+const ServicesHowWeWork = () => {
+  return (
+    <section className="services-how-we-work">
+      <div className="container">
+        <div className="services-how-we-work-wrap">
+          <div className="services-how-we-work-heading">
+            <h1>How We Work</h1>
+            <span>
+              It is a long established fact will be distracted.Lorem Ipsum is
+              simply dummy text of the printing and typesetting industry.
+            </span>
+          </div>
+          <div className="services-how-we-work-carts">
+            {ServicesHowWeWorkData.map((service, index) => (
+              <div className="services-how-we-work-cart" key={index}>
+                <img
+                  className="services-how-we-work-cart-img"
+                  src={service.image}
+                  alt="img"
+                ></img>
+                <div className="services-how-we-work-cart-wrap">
+                  <div className="services-how-we-work-cart-title">
+                    <img
+                      className="services-how-we-work-cart-title-img"
+                      src={`img/services-page/${service.icon}`}
+                      alt="img"
+                    ></img>
+                    <div className="count-cart">{service.count}</div>
+                  </div>
+                  <div className="services-how-we-work-cart-text">
+                    <h2>{service.title}</h2>
+                    <p>{service.description}</p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default ServicesHowWeWork;
---
+++ src/components/Arrays/ServicesHowWeWorkData.js
@@ -0,0 +1,36 @@
+const ServicesHowWeWorkData = [
+  {
+    count: "01",
+    title: "Concept & Details",
+    description:
+      "It is a long established fact will be distracted. Lorem Ipsum is simply dummy from text of the and typesetting indufstry.",
+    image: "img/services-page/carts/services-concept-details.jpg",
+    icon: "icon/concept-and-details.svg",
+  },
+  {
+    count: "02",
+    title: "How We Work",
+    description:
+      "It is a long established fact will be distracted. Lorem Ipsum is simply dummy from text of the and typesetting indufstry.",
+    image: "img/services-page/carts/services-idea-for-work.jpg",
+    icon: "icon/idea-for-work.svg",
+  },
+  {
+    count: "03",
+    title: "design",
+    description:
+      "It is a long established fact will be distracted. Lorem Ipsum is simply dummy from text of the and typesetting indufstry.",
+    image: "img/services-page/carts/services-design.jpg",
+    icon: "icon/design.svg",
+  },
+  {
+    count: "04",
+    title: "Perfection",
+    description:
+      "It is a long established fact will be distracted. Lorem Ipsum is simply dummy from text of the and typesetting indufstry.",
+    image: "img/services-page/carts/services-perfection.jpg",
+    icon: "icon/perfection.svg",
+  },
+];
+
+export default ServicesHowWeWorkData;
